9db2bc1837ffc95e2c0674b09af845802c35aa98
wanglei <wanglei@beebank.com>
2019-11-05 18:49:47 +0800

    by lay 20191105 扒数据

--- src/main/java/com/wanglei/springbootdemo/controller/TestController2.java
+++ src/main/java/com/wanglei/springbootdemo/controller/TestController2.java
@@ -1,21 +1,30 @@
 package com.wanglei.springbootdemo.controller;
 
+import com.wanglei.springbootdemo.util.PdfProcessUtil;
+
+import java.io.File;
+import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TestController2 {
     public static void main(String[] args) {
-        String a = "25. Blain P, Paques M, Massin P, et al. Epiretinal membranes surrounding idiopathic macular holes. Retina \n" +
-                "1998;18:316-21.";
-        String[] str = a.split("\\.");
-        for(String s:str){
-            System.out.println(s);
-        }
+//        String a = "vI. REFERENCES\n" +
+//                "\n" +
+//                "1.";
+//
+//        Pattern p = Pattern.compile("R(EFERENCES|eferences)[\\s\\S]*?1\\.");
+//        Matcher m = p.matcher(a);
+//        while(m.find()) {
+//            System.out.println(m.group());
+//            System.out.println(m.start());
+//        }
 
-        Pattern p = Pattern.compile("[0-9]{4}\\;");
-        Matcher m = p.matcher(a);
-        while(m.find()) {
-            System.out.println(m.start());
+
+        LinkedList<File> list = PdfProcessUtil.folderMethod1("/Users/mfhj-dz-001-068/pythonData/getFromPdf/guide",".pdf");
+        System.out.println(list.size());
+        for(File file:list){
+            System.out.println(file.getName());
         }
     }
 }
--- src/main/java/com/wanglei/springbootdemo/util/PdfProcessUtil.java
+++ src/main/java/com/wanglei/springbootdemo/util/PdfProcessUtil.java
@@ -5,6 +5,8 @@ import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * pdf文件处理
@@ -44,6 +46,71 @@ public class PdfProcessUtil {
         }
 
         return "";
+    }
+
+    /**
+     * @Author lay
+     * @Description
+     * @Date 17:26 2019-11-05
+     * @Param [path, fileType]
+     * @return List
+     **/
+    public static LinkedList<File> folderMethod1(String path,String fileType) {
+
+        File file = new File(path);
+        LinkedList<File> list = new LinkedList<>();
+
+        //保存所有pdf文件的对象
+        LinkedList<File> pdfList = new LinkedList<File>();
+
+        //该路径对应的文件或文件夹是否存在
+        if (file.exists()) {
+
+            //如果该路径为---文件或空文件夹
+            if (null == file.listFiles()) {
+//            	System.out.println(file.getAbsolutePath());
+                if (file.getAbsolutePath().endsWith(fileType))
+                    pdfList.add(file);
+            }
+
+            //如果该路径为非空文件夹
+            else {
+                //将该路径下的所有文件（文件或文件夹）对象加入队列
+                list.addAll(Arrays.asList(file.listFiles()));
+                //遍历该队列
+                while (!list.isEmpty()) {
+
+                    File firstF = list.removeFirst();
+
+                    //这里不论是文件夹还是文件，只需判断是否以“.pdf”结尾
+                    if (firstF.getAbsolutePath().endsWith(".pdf"))
+                        pdfList.add(firstF);
+
+                    File[] files = firstF.listFiles();
+
+                    if (null == files) {
+                        //System.out.println(firstF.getAbsolutePath());
+                        continue;
+                    }
+                    for (File f : files) {
+                        if (f.isDirectory()) {
+                            //System.out.println("文件夹:" + f.getAbsolutePath());
+                            list.add(f);
+                        } else {
+                            //System.out.println("文件:" + f.getAbsolutePath());
+
+                            if (f.getAbsolutePath().endsWith(".pdf"))
+                                pdfList.add(f);
+
+                        }
+                    }
+                }
+            }
+
+        } else {
+            System.out.println("文件不存在!");
+        }
+        return pdfList;
     }
 
 }
