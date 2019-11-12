77a1fbd9419f7c2ac5b8b38b9bbe4e3701b00241
wangl <wanglei19930915@126.com>
2019-11-13 01:03:58 +0800

    新的修改 add 解析word by lay 20191113

--- src/main/java/com/wanglei/springbootdemo/controller/TestControllerForWin1.java
+++ src/main/java/com/wanglei/springbootdemo/controller/TestControllerForWin1.java
@@ -11,9 +11,11 @@ import java.util.regex.Pattern;
 
 public class TestControllerForWin1 {
 
+    public static final String REG = "([hH][tT]{2}[pP]:/*|[hH][tT]{2}[pP][sS]:/*|[fF][tT][pP]:/*)(([A-Za-z0-9-~]+).)+([A-Za-z0-9-~\\/])+(\\?{0,1}(([A-Za-z0-9-~]+\\={0,1})([A-Za-z0-9-~]*))*)";
+
     public static void main(String[] args) throws Exception{
         //获取所有pdf文件路径的list
-        LinkedList<File> fileList = PdfProcessUtil.folderMethod1("D:\\pythonData\\pdfData\\1",".pdf");
+        LinkedList<File> fileList = PdfProcessUtil.folderMethod1("D:\\pythonData\\guide",".docx");
         System.out.println("文件list长度位："+fileList.size());
         int sumRows = 0;
         for(File file:fileList){
@@ -36,7 +38,7 @@ public class TestControllerForWin1 {
         String parentFilename = file.getParentFile().getName();
         try {
             List<Map<String, String>> list = new ArrayList();
-            String content = PdfProcessUtil.pdfToString(file);
+            String content = PdfProcessUtil.getPhoneNum(file);
             //根据正则匹配到References的开头
             Pattern p = Pattern.compile("R(EFERENCES|eferences)[\\s\\S]*?1\\.");
             Matcher mm = p.matcher(content);
@@ -47,8 +49,10 @@ public class TestControllerForWin1 {
 //        System.out.println(content.substring(a).replaceAll("guide.medlive.cn" ,"").replaceAll("Idiopathic Macular Hole PPP: ","").replaceAll("References",""));
             String subC = content.substring(start + 10).replaceAll("guide.medlive.cn", "").replaceAll("Idiopathic Macular Hole PPP: ", "").replaceAll("References", "");
 //        System.out.println(subC);
+            subC = subC.replaceAll(REG,"");
 //        String pattern = "[0-9]+\\..*?[0-9]+\\;.*?\\.";
-            String pattern = "[0-9]+\\.[\\s\\S]*?[0-9]+\\;.*?\\.|http.*?[0-9]+\\.";
+//            String pattern = "([0-9]{1}|[0-9]{2}|[0-9]{3})\\.[\\s\\S]*?[0-9]{4}(\\;[\\s\\S]*?\\.|\\:[\\s\\S]*?\\.|\\.)|http[\\s\\S]*?[0-9]+\\.";
+            String pattern = "([0-9]{1}|[0-9]{2}|[0-9]{3})\\.[\\s\\S]*?([0-9]{4}\\;[\\s\\S]*?\\.|[0-9]{4}\\:[\\s\\S]*?\\.|\\, [0-9]{4}\\.)";
             Pattern re = Pattern.compile(pattern);
             Matcher m = re.matcher(subC);
             //创建一个dataMap装载数据
@@ -59,7 +63,7 @@ public class TestControllerForWin1 {
                 dataMap.put("文件名", fileName);
                 dataMap.put("名字", str[1]);
                 String title = "";
-                for (int i = 2; i < len - 1; i++) {
+                for (int i = 2; i < len - 2; i++) {
                     title = title + str[i];
                 }
                 dataMap.put("标题", title);
@@ -67,7 +71,7 @@ public class TestControllerForWin1 {
                 //处理year，year中包含机构、年份、期刊号
                 String yearAndCode = str[len - 1].trim();
                 int yearStartIndex = getCodeEndIndex(yearAndCode);
-                String code = yearAndCode.substring(0, yearStartIndex);
+                String code = str[len-2];
                 String year = yearAndCode.substring(yearStartIndex, yearStartIndex + 4);
                 String issueNo = yearAndCode.substring(yearStartIndex);
                 dataMap.put("机构", code);
@@ -80,7 +84,7 @@ public class TestControllerForWin1 {
             sheetName.add("1");
             String[] title = {"文件名", "名字", "标题", "机构", "年份", "刊号"};
             //开始写文件
-            String fileDir = "D:\\pythonData\\toExcel\\"+parentFilename+fileName+".xls";
+            String fileDir = "D:\\pythonData\\export\\"+parentFilename+"\\"+fileName+".xls";
             File goalFile = new File(fileDir);
             if (!goalFile.exists()) {
                 WriteExcelUtil.createExcelXls(fileDir, sheetName, title);
@@ -100,7 +104,7 @@ public class TestControllerForWin1 {
      * @return
      */
     public static int getCodeEndIndex(String str){
-        Pattern p = Pattern.compile("[0-9]{4}\\;");
+        Pattern p = Pattern.compile("(19|20)[1-9]{2}");
         Matcher m = p.matcher(str);
         int index = 0;
         while(m.find()) {
